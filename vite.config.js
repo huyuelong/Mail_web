942882d47d4610662e02966e18b95bc1417d6767
huyuelong <949910047@qq.com>
2024-03-14 23:40:56 +0800

    主题色定制样式

--- vite.config.js
+++ vite.config.js
@@ -14,17 +14,28 @@ export default defineConfig({
   plugins: [
     vue(),
     VueDevTools(),
-    
+
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      // 1.配置elementPlus为sass样式
+      resolvers: [ElementPlusResolver({ importStyle: "sass" })],
     }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 2.自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
 })
