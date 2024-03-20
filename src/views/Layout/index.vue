6b904c909e12234f9691875781d0281b31ea44e3
huyuelong <949910047@qq.com>
2024-03-20 14:43:41 +0800

    一级导航渲染

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,7 +1,12 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
+
 <template>
-    <div>
-        首页
-        <!-- 二级路由出口 -->
-        <RouterView></RouterView>
-    </div>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
 </template>
